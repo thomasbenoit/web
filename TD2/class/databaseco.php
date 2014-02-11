e2dc473ea4d1c661356b97f24b54ede7b47064c0
Benoit Poulain <benoit.poulain@etu.univ-orleans.fr>
2014-02-11 08:09:01 +0100

    Correction des fichiers de connexion à la base de données

--- TD2/class/databaseco.php
+++ TD2/class/databaseco.php
@@ -1,8 +1,8 @@
 <?php
-$server='localhost';
-$user='root';
-$pass='';
-$bdd='dbrangeard';
+$server='info2';
+$user='rangeard';
+$pass='rangeard';
+$bdd='DBrangeard';
 $port='3306';
 try{
 	$cnx=new PDO('mysql:host='.$server.';port='.$port.';dbname='.$bdd,$user,$pass);
